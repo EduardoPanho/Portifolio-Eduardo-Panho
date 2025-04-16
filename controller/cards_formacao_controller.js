47b091c9d35211a943488eae0b38d9ba1bfab8c0
Eduardo Panho - 에드워드 <eduardopanho9@gmail.com>
2025-04-16 17:29:16 -0300

    Portifólio - botoes das pag

---
+++ controller/cards_formacao_controller.js
@@ -0,0 +1,21 @@
+import { bd_cards_formacao } from "./../model/cards_formacao_bd.js"; 
+
+export function createCards() {
+    let cards_section = document.getElementById("cards");
+
+    bd_cards_formacao.forEach((card_bd) => {
+        let card = document.createElement("div");
+        card.className = "card";
+
+        let titulo = document.createElement("h1");
+        titulo.textContent = card_bd.titulo;
+
+        let preco = document.createElement("h3");
+        preco.textContent = card_bd.preco;
+
+        card.appendChild(titulo);
+        card.appendChild(preco);
+
+        cards_section.appendChild(card);
+    })
+}
