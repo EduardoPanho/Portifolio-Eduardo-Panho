44fb2eb523f6dc81f642d17d7365f79bd79d7ee2
Eduardo Panho - 에드워드 <eduardopanho9@gmail.com>
2026-04-07 16:07:20 -0300

    maiusculo

--- model/cards_formacao_bd.js
+++ model/cards_formacao_bd.js
@@ -1,6 +1,6 @@
 export const bd_cards_formacao = [
-    { titulo: "Francisco Beltrão", preco: "Unipar", imagem: "assets/education/div_img_1.jpg" },
-    { titulo: "formação - Sistemas", preco: "Carga horária em andamento", imagem: "./../assets/education/div_img_2.png" },
-    { titulo: "Francisco Beltrão", preco: "Senac", imagem: "./../assets/education/div_img_3.png" },
-    { titulo: "formação - Programação web", preco: "Carga horária - 240h", imagem: "./../assets/education/div_img_4.png" },
+    { titulo: "Francisco Beltrão", preco: "Unipar", imagem: "../Assets/education/div_img_1.jpg" },
+    { titulo: "formação - Sistemas", preco: "Carga horária em andamento", imagem: "../Assets/education/div_img_2.png" },
+    { titulo: "Francisco Beltrão", preco: "Senac", imagem: "../Assets/education/div_img_3.png" },
+    { titulo: "formação - Programação web", preco: "Carga horária - 240h", imagem: "../Assets/education/div_img_4.png" },
 ];
